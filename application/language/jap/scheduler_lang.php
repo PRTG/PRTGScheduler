62b55d2253a190821a543d57b80709e82b3ec7ad
Stephan Linke <stephan.linke@paessler.com>
2017-06-22 16:28:56 +0200

    Updated japanese translation

--- application/language/jap/scheduler_lang.php
+++ application/language/jap/scheduler_lang.php
@@ -4,7 +4,7 @@
 $lang['menu_entry'] = "メンテナンスウィンドウ";
 
 # headline
-$lang['configReport_headLine']  = '構成レポート：メンテナンスウィンドウ';
+$lang['configReport_headLine']  = '構成レポート：*メンテナンスウィンドウ';
 
 # table headlines
 $lang['tableHeadline_sensors']  = 'センサー';
@@ -16,24 +16,24 @@ $lang['table_objid']            = 'オブジェクトID';
 $lang['table_name']             = '名前';
 $lang['table_group']            = 'グループ';
 $lang['table_probe']            = 'プローブ';
-$lang['table_nextMaintenance']  = '次のメンテナンス';
-$lang['table_nextStart']        = '次の開始';
-$lang['table_nextEnd']          = '次のエンド';
+$lang['table_nextMaintenance']  = '次回のメンテナンス';
+$lang['table_nextStart']        = '次の開始日時';
+$lang['table_nextEnd']          = '次の終了日時';
 $lang['table_duration']         = '期間';
 $lang['table_description']      = '説明';
-$lang['table_recurrence']       = '反復';
-$lang['table_once']             = '一度';
-$lang['table_noMaintenances']   = '設定されたメンテナンスウィンドウが見つかりません！';
+$lang['table_recurrence']       = '繰り返し';
+$lang['table_once']             = '1回のみ';
+$lang['table_noMaintenances']   = '設定された*メンテナンスウィンドウ*が見つかりません！';
 
 
 $lang['duration_minutes']   = '分';
-$lang['duration_hours']     = '時間';
-$lang['duration_days']      = '日々';
+$lang['duration_hours']     = '時';
+$lang['duration_days']      = '日';
 $lang['duration_months']    = '月';
 $lang['duration_years']     = '年';
 
 # prtg channel names
-$lang['channel_sensorMaintenances']    = 'センサのメンテナンス';
+$lang['channel_sensorMaintenances']    = 'センサーのメンテナンス';
 $lang['channel_deviceMaintenances']    = 'デバイスメンテナンス';
 $lang['channel_groupMaintenances']     = 'グループメンテナンス';
 $lang['channel_totalMaintenances']     = 'トータルメンテナンス';
